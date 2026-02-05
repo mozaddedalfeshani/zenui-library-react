c33f9a780a01ac5d1f952de0cbb3b596d821d89d
mozaddedalfeshani <murad15-6047@diu.edu.bd>
2026-02-05 15:16:30 +0600

    feat: enhance i18n support in NewsBoard component with translations for prefix, link, and suffix

--- src/Components/Home/NewsBoard.jsx
+++ src/Components/Home/NewsBoard.jsx
@@ -1,14 +1,21 @@
 import {FiArrowUpRight} from "react-icons/fi";
 
+// i18n
+import { useTranslation } from 'react-i18next';
+
 const NewsBoard = () => {
+    const { t } = useTranslation();
+
     return (
         <section className="bg-brandColor flex items-center justify-center gap-1 w-full px-5 py-3 md:h-[50px]">
-            <p className="text-base text-white text-center font-medium">🔥 We just launched <a
+            <p className="text-base text-white text-center font-medium">
+                {t('home.newsboard_prefix')}
+                <a
                 href="https://vueui.zenui.net"
                 target="_blank"
                 rel="noreferrer"
-                className="hover:underline">ZenUI
-                Library Vue!</a> Don’t miss it.
+                className="hover:underline">{t('home.newsboard_link')}</a>
+                {t('home.newsboard_suffix')}
                 <FiArrowUpRight className='text-white text-[1.4rem] inline ml-1'/>
             </p>
         </section>
